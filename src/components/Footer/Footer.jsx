48b143f168dc81017b710ac085bb1956a9fb08a0
Jan Phandoh <janphandoh@gmail.com>
2024-08-21 08:41:59 +0000

    added and styled all components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,51 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <section className="gramophone">
+        <h3>Gramophone Academy of Music</h3>
+        <div className="social-links">
+          <a href="">
+            <img src="src\assets\icons\facebook-2.png" alt="" />
+          </a>
+          <a href="">
+            <img src="src\assets\icons\linkedin.png" alt="" />
+          </a>
+          <a href="">
+            <img src="src\assets\icons\youtube.png" alt="" />
+          </a>
+          <a href="">
+            <img src="src\assets\icons\instagram.png" alt="" />
+          </a>
+        </div>
+      </section>
+      {/* <section className="topic">
+        <h4>Topic</h4>
+        <ul>
+          <li>Page</li>
+          <li>Page</li>
+          <li>Page</li>
+        </ul>
+      </section>
+      <section className="topic">
+        <h4>Topic</h4>
+        <ul>
+          <li>Page</li>
+          <li>Page</li>
+          <li>Page</li>
+        </ul>
+      </section>
+      <section className="topic">
+        <h4>Topic</h4>
+        <ul>
+          <li>Page</li>
+          <li>Page</li>
+          <li>Page</li>
+        </ul>
+      </section> */}
+    </footer>
+  );
+};
+
+export default Footer;
