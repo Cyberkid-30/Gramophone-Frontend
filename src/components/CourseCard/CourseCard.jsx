f1481ef2596554e94e30936f9814fdcbeff8401e
Ampmusicsog <aaamo-mensah@st.ug.edu.gh>
2024-08-23 15:43:09 +0000

    Added images

--- src/components/CourseCard/CourseCard.jsx
+++ src/components/CourseCard/CourseCard.jsx
@@ -6,7 +6,7 @@ import "../../pages/StudentCourses/StudentCourses.css";
 // Function to get image URL based on courseCode
 const getImageURL = (courseCode) => {
   if (courseCode.startsWith("Drum")) {
-    return "Gramophone-backend/assets/pexels-juresiric-730656.jpg"; // Path to drum image
+    return "public/assets/Images/pexels-juresiric-730656.jpg"; // Path to drum image
   }
   // Add more conditions for other course types if needed
   // For example:
@@ -24,7 +24,7 @@ const CourseCard = ({ course }) => {
   useEffect(() => {
     const fetchAnnouncements = async () => {
       try {
-        const response = await fetch(`http://localhost:4000/courses/${course.courseCode}/announcements`); // Adjust URL as needed
+        const response = await fetch(`https://ampsgramophone-backend.vercel.app/courses/${course.courseCode}/announcements`); // Adjust URL as needed
         const data = await response.json();
         if (response.ok) {
           setAnnouncements(data.announcements);
