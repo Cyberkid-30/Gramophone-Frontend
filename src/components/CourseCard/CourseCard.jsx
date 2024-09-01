af4a6dcc2486d610237e03a9d825c2212c526123
Ampmusicsog <aaamo-mensah@st.ug.edu.gh>
2024-09-01 23:28:34 +0000

    Test1

--- src/components/CourseCard/CourseCard.jsx
+++ src/components/CourseCard/CourseCard.jsx
@@ -6,7 +6,7 @@ import "../../pages/StudentCourses/StudentCourses.css";
 // Function to get image URL based on courseCode
 const getImageURL = (courseCode) => {
   if (courseCode.startsWith("Drum")) {
-    return "/assets/Images/pexels-juresiric-730656.jpg"; // Path to drum image
+    return "/assets/Images/Drum PIC.jpg"; // Path to drum image
   }
   // Add more conditions for other course types if needed
   // For example:
