2dc34421c826596bf28e6eba38de56634d850d9d
Ampmusicsog <aaamo-mensah@st.ug.edu.gh>
2024-09-02 02:03:57 +0000

    Test 8

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-/* eslint-disable no-unused-vars */
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
-import LandingPage from "././pages/LandingPage/LandingPage";
+import LandingPage from "./pages/LandingPage/LandingPage";
 import StudentCourses from "./pages/StudentCourses/StudentCourses";
-import Dashboard from "././pages/DashboardPage/Dashboard";
+import Dashboard from "./pages/DashboardPage/Dashboard";
 import LoginPage from "./pages/LandingPage/Modal";
-import ContactUs from "././pages/ContactPage/ContactUs";
+import ContactUs from "./pages/ContactPage/ContactUs";
 import StudentDashboard from "./pages/StudentDashboard/StudentDashboard";
 import InstructorDashboard from "./pages/InstructorDashboard/InstructorDashboard";
 import InstructorCourses from "./pages/InstructorCourses/InstructorCourses";
@@ -19,16 +18,16 @@ function App() {
     <div className="container">
       <Router>
         <Routes>
-          <Route path="/" Component={LandingPage} />
-          <Route path="/loggedIn" Component={StudentCourses} />
-          <Route path="/dashboard" Component={Dashboard} />
-          <Route path="/login" Component={LoginPage} />
-          <Route path="/contactUs" Component={ContactUs} />
-          <Route path="/studentDashboard" Component={StudentDashboard} />
-          <Route path="/instructorDashboard" Component={InstructorDashboard} />
-          <Route path="/instructorCourses" Component={InstructorCourses} />
-          <Route path="/adminDashboard" Component={AdminDashboard} />
-          <Route path="/adminPage" Component={AdminStudentTab} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/loggedIn" element={<StudentCourses />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/contactUs" element={<ContactUs />} />
+          <Route path="/studentDashboard" element={<StudentDashboard />} />
+          <Route path="/instructorDashboard" element={<InstructorDashboard />} />
+          <Route path="/instructorCourses" element={<InstructorCourses />} />
+          <Route path="/adminDashboard" element={<AdminDashboard />} />
+          <Route path="/adminPage" element={<AdminStudentTab />} />
         </Routes>
         <Footer />
       </Router>
